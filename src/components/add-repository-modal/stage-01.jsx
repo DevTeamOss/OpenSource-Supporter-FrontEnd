e7684308ac85a2d053b5b001bcb77eb4eea9926b
kmwyatt <arc2437@naver.com>
2024-05-08 20:45:23 +0900

    design: Add design of base modal

---
+++ src/components/add-repository-modal/stage-01.jsx
@@ -0,0 +1,3 @@
+export default function Stage01({ nextStage }) {
+    return <button onClick={nextStage}>nextStage</button>
+}
--- src/pages/base-page/aside-right.jsx
+++ src/pages/base-page/aside-right.jsx
@@ -8,39 +8,58 @@ import {
     faUser,
 } from '@fortawesome/free-solid-svg-icons'
 
+import { useModal } from '@/hooks/useModal.js'
+import AddRepositoryModal from '@/components/add-repository-modal/index.jsx'
+
 export default function AsideRight() {
     const navigate = useNavigate()
 
+    const addRepositoryModal = useModal()
+
     return (
-        <div className="aside-right-container">
-            <div className="menu-box">
-                <div className="menu-item" onClick={() => navigate('/profile')}>
-                    <FontAwesomeIcon icon={faUser} />
-                    <div className="menu-item-text">Your Profile</div>
+        <>
+            <div className="aside-right-container">
+                <div className="menu-box">
+                    <div
+                        className="menu-item"
+                        onClick={() => navigate('/profile')}
+                    >
+                        <FontAwesomeIcon icon={faUser} />
+                        <div className="menu-item-text">Your Profile</div>
+                    </div>
+                    <div
+                        className="menu-item"
+                        onClick={addRepositoryModal.open}
+                    >
+                        <FontAwesomeIcon icon={faBook} />
+                        <div className="menu-item-text">Your Repositories</div>
+                    </div>
+                    <div className="menu-item">
+                        <FontAwesomeIcon icon={faGlobe} />
+                        <div className="menu-item-text">Your Links</div>
+                    </div>
+                    <div className="menu-item">
+                        <FontAwesomeIcon icon={faCoins} />
+                        <div className="menu-item-text">Your Points</div>
+                    </div>
+                    <div className="menu-item">
+                        <FontAwesomeIcon icon={faTrophy} />
+                        <div className="menu-item-text">Your Rank</div>
+                    </div>
+                    <div className="divide-line" />
+                    <div className="menu-item">Opensource Licenses</div>
+                    <div className="divide-line" />
+                    <div
+                        className="menu-item red"
+                        onClick={() => navigate('/')}
+                    >
+                        Logout
+                    </div>
                 </div>
-                <div className="menu-item">
-                    <FontAwesomeIcon icon={faBook} />
-                    <div className="menu-item-text">Your Repositories</div>
-                </div>
-                <div className="menu-item">
-                    <FontAwesomeIcon icon={faGlobe} />
-                    <div className="menu-item-text">Your Links</div>
-                </div>
-                <div className="menu-item">
-                    <FontAwesomeIcon icon={faCoins} />
-                    <div className="menu-item-text">Your Points</div>
-                </div>
-                <div className="menu-item">
-                    <FontAwesomeIcon icon={faTrophy} />
-                    <div className="menu-item-text">Your Rank</div>
-                </div>
-                <div className="divide-line" />
-                <div className="menu-item">Opensource Licenses</div>
-                <div className="divide-line" />
-                <div className="menu-item red" onClick={() => navigate('/')}>
-                    Logout
-                </div>
+                {addRepositoryModal.visible && (
+                    <AddRepositoryModal close={addRepositoryModal.close} />
+                )}
             </div>
-        </div>
+        </>
     )
 }
