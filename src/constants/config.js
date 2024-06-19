c984908ef500a75d872a1e059ffc2e56690286df
kmwyatt <arc2437@naver.com>
2024-06-19 19:48:09 +0900

    chore: Add redux persist

--- src/constants/config.js
+++ src/constants/config.js
@@ -1,2 +1,3 @@
 export const BACKEND_URL = import.meta.env.VITE_BACKEND_URL
 export const GITHUB_AUTH_URL = import.meta.env.VITE_GITHUB_AUTH_URL
+export const REDUX_LOCAL_STORAGE = import.meta.env.VITE_REDUX_LOCAL_STORAGE
